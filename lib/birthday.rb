00fe823a8b7ab3441416531d07eebb4755f287b4
Sharnell Grant <sharnellgrant@gmail.com>
2020-02-20 01:30:37 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -13,8 +13,7 @@ end
 def age_appropriate_birthday(birthday_kids)
   birthday_kids.each do |name, age|
     if age <= 12
-      puts "Happy Birthday #{name}! You are now #{age} years old!" 
+      puts "Happy Birthday #{name}! You are now #{age} years old!" "You are too old for this"
     end
-    puts 
   end
 end  
